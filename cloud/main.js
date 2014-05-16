d416134bc8a1c778d908af0a8551042b2dca0d42
zjc1985 <lionhart586@hotmail.com>
2014-05-15 22:04:45 -0700

    test dependecy

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,6 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success(sayHi());
 });
+
---
+++ cloud/test.js
@@ -0,0 +1,3 @@
+function sayHi(){
+	return "Hello , This is mapmarker cloud";
+}
