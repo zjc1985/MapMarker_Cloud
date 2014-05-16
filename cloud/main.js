ba8c4e3b5a393e3031ef5392ee208bd328617db0
zjc1985 <lionhart586@hotmail.com>
2014-05-15 22:26:33 -0700

    add web hosting support

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,6 @@
+require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  var test = require('cloud/test.js');
-  response.success(test.sayHi());
+  response.success("Hello world!");
 });
-
